60faf05c62645cb03433b92af457c0fd797c8965
Abdo <abdo@axetay.co.uk>
2020-04-21 00:26:25 +0100

    read from repo information environment variables

--- src/PipeLineTemplate/bin/buildroom.ts
+++ src/PipeLineTemplate/bin/buildroom.ts
@@ -5,16 +5,15 @@ import { BuildOperationsDetails } from '../lib/buildOperationsDetails';
 
 const app = new cdk.App();
 const  buildOperationsDetails : BuildOperationsDetails = {
-    "githubRepositoryName" : "AngleRunners",
-    "githubRepositoryOwner" : "AbdoNile",
-    "githubRepositoryBranch" : "dev",
-    "projectName" : "AngleRunnerViaCDK",
+    "githubRepositoryName" : `${process.env.githubRepositoryName}`,
+    "githubRepositoryOwner" :`${process.env.githubRepositoryOwner}`,
+    "githubRepositoryBranch" :`${process.env.githubRepositoryBranch}`,
+    "projectName" : `${process.env.githubRepositoryName}-${process.env.githubRepositoryBranch}`,
     "buildSpecFileRelativeLocation" : "frontend/build/codebuild.spec.yml",
     "artifactsBucket" : "anglerunner-artifacts",
     "buildAsRole" : "arn:aws:iam::101584550521:role/service-role/codebuild-AngleRunner-service-role",
     "gitHubTokenSecretName" :  "githubtoken"
-
-  }
+}
 
 const stackName = `${buildOperationsDetails.projectName}-${buildOperationsDetails.githubRepositoryBranch}`
 new BuildroomStack(app, stackName, buildOperationsDetails);
