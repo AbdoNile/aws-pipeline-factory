5c0b5e93f17703b71f629cbdbfdf07b50d70f362
Abdo <abdo@axetay.co.uk>
2020-04-21 19:13:23 +0100

    fix the CodeBuilde

--- src/PipeLineTemplate/lib/buildroom-stack.ts
+++ src/PipeLineTemplate/lib/buildroom-stack.ts
@@ -8,9 +8,9 @@ import {BuildOperationsDetails} from "./buildOperationsDetails"
 export class BuildroomStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: BuildOperationsDetails) {
     super(scope, id, props);
-    const buildIamROle = new buildRunnerIamRole.BuildRunnerIamRole(this, "CodeBuilde" , props )
+    const buildIamROle = new buildRunnerIamRole.BuildRunnerIamRole(this, "IAMRole" , props )
  
-    const builder = new codeBuilder.CodeBuilder(this, "CodeBuilde" , props,buildIamROle.role )
+    const builder = new codeBuilder.CodeBuilder(this, "CodeBuilder" , props,buildIamROle.role )
     const pipLine = new codePiplineer.CodePipeline(this, "CodePipeLine", props, builder.buildProjectArn, buildIamROle.role)
   }
 
