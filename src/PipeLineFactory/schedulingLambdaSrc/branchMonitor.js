addbf817e6a4a169e8895a26ca9a06ebf7c18c78
Abdo <abdo@axetay.co.uk>
2020-04-30 09:23:34 +0100

    making lambda ready for  multiple entry points

--- src/PipeLineFactory/schedulingLambdaSrc/branchMonitor.js
+++ src/PipeLineFactory/schedulingLambdaSrc/branchMonitor.js
@@ -6,6 +6,22 @@ function getBranchNamefromRef(refvalue){
 
 }
 
+exports.handleApiRequest =  function(event) {
+  var payload = event;
+  console.debug(payload);
+ 
+  var buildParameter = {
+    branchaction : "action",
+    changeset : githubContext.after,
+   repository : {
+        name:  githubContext.repository.name,
+        owner: githubContext.repository.owner.login,
+        branch : getBranchNamefromRef(githubContext.ref)
+    }
+  }
+
+  TriggerProject(buildParameter)
+}
 
 exports.handleGitHubMessage =  function(event) {
     var payload = event.Records[0].Sns.Message;
@@ -31,58 +47,63 @@ exports.handleGitHubMessage =  function(event) {
       }
     }
 
-    console.debug(buildParameter);  
-    var codebuild = new AWS.CodeBuild({apiVersion: '2016-10-06'});
-    
-    var buildProjectName = process.env.FactoryCodeBuildProjectName;
-    console.debug(buildProjectName);  
-   
-    var params =
-    {
-      projectName: buildProjectName,
-      environmentVariablesOverride: [
-        {
-          name: 'GITHUB_REPOSITORY_NAME', 
-          value: buildParameter.repository.name, 
-          type: "PLAINTEXT" 
-        },
-        {
-          name: 'GITHUB_REPOSITORY_BRANCH', 
-          value: buildParameter.repository.branch, 
-          type: "PLAINTEXT" 
-        },
-        {
-          name: 'GITHUB_REPOSITORY_OWNER', 
-          value: buildParameter.repository.owner, 
-          type: "PLAINTEXT" 
-        },
-        {
-          name: 'BUILD_SPEC_RELATIVE_LOCATION', 
-          value: "buildspec.yml", 
-          type: "PLAINTEXT" 
-        },
-        {
-          name: 'ARTIFACTS_BUCKET', 
-          value: "salt-deployment-packages", 
-          type: "PLAINTEXT" 
-        },
-        {
-          name: 'GITHUB_TOKEN_SECRETNAME', 
-          value: "GitHubToken", 
-          type: "PLAINTEXT" 
-        },
-        {
-          name: 'ARTIFACTS_PREFIX', 
-          value: ``, 
-          type: "PLAINTEXT" 
-        }  
-      ]
-    };
-    console.debug(params);  
-   
-    codebuild.startBuild(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });
+    TriggerProject(buildParameter)
+}
+
+TriggerProject = function(buildParameter){
+
+  console.debug(buildParameter);  
+  var codebuild = new AWS.CodeBuild({apiVersion: '2016-10-06'});
+  
+  var buildProjectName = process.env.FactoryCodeBuildProjectName;
+  console.debug(buildProjectName);  
+ 
+  var params =
+  {
+    projectName: buildProjectName,
+    environmentVariablesOverride: [
+      {
+        name: 'GITHUB_REPOSITORY_NAME', 
+        value: buildParameter.repository.name, 
+        type: "PLAINTEXT" 
+      },
+      {
+        name: 'GITHUB_REPOSITORY_BRANCH', 
+        value: buildParameter.repository.branch, 
+        type: "PLAINTEXT" 
+      },
+      {
+        name: 'GITHUB_REPOSITORY_OWNER', 
+        value: buildParameter.repository.owner, 
+        type: "PLAINTEXT" 
+      },
+      {
+        name: 'BUILD_SPEC_RELATIVE_LOCATION', 
+        value: "buildspec.yml", 
+        type: "PLAINTEXT" 
+      },
+      {
+        name: 'ARTIFACTS_BUCKET', 
+        value: "salt-deployment-packages", 
+        type: "PLAINTEXT" 
+      },
+      {
+        name: 'GITHUB_TOKEN_SECRETNAME', 
+        value: "GitHubToken", 
+        type: "PLAINTEXT" 
+      },
+      {
+        name: 'ARTIFACTS_PREFIX', 
+        value: ``, 
+        type: "PLAINTEXT" 
+      }  
+    ]
+  };
+  console.debug(params);  
+ 
+  codebuild.startBuild(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
 
 }
