0e036837fc29b2af2b2051ab3bcc4e56b51b1bf6
Justas Veršekys <justas.versekys@telesoftas.com>
2020-05-18 12:07:43 +0300

    refactor: review comments

---
+++ src/PipeLineTemplate/lib/notification.ts
@@ -0,0 +1,28 @@
+import * as cdk from "@aws-cdk/core";
+import * as chatbot from '@aws-cdk/aws-chatbot';
+import * as ssm from '@aws-cdk/aws-ssm';
+import { BuildOperationsDetails } from "./buildOperationsDetails";
+import { IPipeline } from "@aws-cdk/aws-codepipeline";
+
+export class Notification extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string, props: BuildOperationsDetails, pipeline: IPipeline) {
+        super(scope, id);
+        
+        let slackChannelName = props.slackChannelNamePrefix;
+        if(props.githubRepositoryBranch === 'master' || props.githubRepositoryBranch.startsWith('bumastemra')){
+            slackChannelName += props.githubRepositoryBranch;
+        } else {
+            slackChannelName += 'other';
+        }
+
+        let slackChannelId = ssm.StringParameter.valueForSecureStringParameter(scope, slackChannelName, 1);
+
+        new chatbot.CfnSlackChannelConfiguration(this, props.projectName + 'SlackChannelConfiguration', {
+            loggingLevel: 'INFO',
+            configurationName: props.projectName + 'CodePipelineStateChangesConfiguration',
+            iamRoleArn: props.buildAsRoleArn,
+            slackChannelId: slackChannelId,
+            slackWorkspaceId: props.slackWorkspaceId,
+          });
+    }
+}
