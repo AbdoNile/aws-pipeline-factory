31a3b2002fd333a7e94685dfdd28d98cbf412e3a
Giedrius Valančius <42999473+gievln@users.noreply.github.com>
2020-05-19 11:13:03 +0300

    feat: add  repository branch and github token secret name to codeBuilder env variables (#11)

--- src/PipeLineTemplate/lib/codeBuilder.ts
+++ src/PipeLineTemplate/lib/codeBuilder.ts
@@ -2,15 +2,15 @@ import * as cdk from "@aws-cdk/core";
 import * as codebuild from "@aws-cdk/aws-codebuild";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as iam from "@aws-cdk/aws-iam";
-import {BuildOperationsDetails} from "./buildOperationsDetails"
+import { BuildOperationsDetails } from "./buildOperationsDetails"
 
 export class CodeBuilder extends cdk.Construct {
-  public readonly buildProjectArn : string  ;
-  constructor(scope: cdk.Construct, id: string, props: BuildOperationsDetails, buildAsRole : iam.IRole ) {
+  public readonly buildProjectArn: string;
+  constructor(scope: cdk.Construct, id: string, props: BuildOperationsDetails, buildAsRole: iam.IRole) {
     super(scope, id);
     var buildSpecFile = props.buildSpecFileRelativeLocation || "buildspec.yml"
 
-    const gitHubSource = codebuild.Source.gitHub( {
+    const gitHubSource = codebuild.Source.gitHub({
       owner: props.githubRepositoryOwner,
       repo: props.githubRepositoryName,
       webhook: false/*,
@@ -22,37 +22,44 @@ export class CodeBuilder extends cdk.Construct {
     });
 
     const artifactsBucket = s3.Bucket.fromBucketName(this, 'CodeBuildArtifactsBucket', props.artifactsBucket);
-     
+
     const codeBuildProject = new codebuild.Project(this, props.projectName, {
       buildSpec: codebuild.BuildSpec.fromSourceFilename(buildSpecFile),
-      role : buildAsRole,
+      role: buildAsRole,
       source: gitHubSource,
       environment: {
-       buildImage: codebuild.LinuxBuildImage.STANDARD_3_0
-      
-      },
-      projectName : `PLF-${props.projectName}`,
-      environmentVariables :  {
-        "STAGE_ENV_NAME" : {
-          value : props.githubRepositoryBranch.toLowerCase(),
-          type : codebuild.BuildEnvironmentVariableType.PLAINTEXT
-        },
-        "STAGE_PACKAGE_BUCKET_NAME" : {
-          value : props.artifactsBucket,
-          type : codebuild.BuildEnvironmentVariableType.PLAINTEXT
-        },
+        buildImage: codebuild.LinuxBuildImage.STANDARD_3_0
 
       },
-      artifacts : codebuild.Artifacts.s3({
-          bucket: artifactsBucket,
-          path :  `${props.artifactsPrefix}`,
-          includeBuildId : true,
-          name: `${props.githubRepositoryName}`,
-          packageZip : true
+      projectName: `PLF-${props.projectName}`,
+      environmentVariables: {
+        "STAGE_ENV_NAME": {
+          value: props.githubRepositoryBranch.toLowerCase(),
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
+        },
+        "STAGE_PACKAGE_BUCKET_NAME": {
+          value: props.artifactsBucket,
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
+        },
+        "GITHUB_TOKEN_SECRETNAME": {
+          value: props.gitHubTokenSecretName,
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
+        },
+        "GITHUB_REPOSITORY_BRANCH": {
+          value: props.githubRepositoryBranch,
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
+        },
+      },
+      artifacts: codebuild.Artifacts.s3({
+        bucket: artifactsBucket,
+        path: `${props.artifactsPrefix}`,
+        includeBuildId: true,
+        name: `${props.githubRepositoryName}`,
+        packageZip: true
       })
     });
-    
- 
+
+
     this.buildProjectArn = codeBuildProject.projectArn;
 
   }
