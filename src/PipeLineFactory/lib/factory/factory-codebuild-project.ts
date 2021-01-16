102bc55a889ebd457986fb2b383f0b4f15e4f44f
Abdo <abdo@axetay.co.uk>
2021-01-16 22:30:16 +0000

    move to BuildEnvironment.STANDARD_4_0.

--- src/PipeLineFactory/lib/factory/factory-codebuild-project.ts
+++ src/PipeLineFactory/lib/factory/factory-codebuild-project.ts
@@ -1,26 +1,30 @@
 import * as cdk from "@aws-cdk/core";
-import {IRole} from "@aws-cdk/aws-iam";
+import { IRole } from "@aws-cdk/aws-iam";
 import * as codebuild from "@aws-cdk/aws-codebuild";
 import FactoryProps from "./factory-props";
 
 export default class FactoryCodeBuildProject extends cdk.Construct {
   BuildProject: codebuild.Project;
-  constructor(scope: cdk.Construct, id: string, props : FactoryProps , codebuildRole : IRole ) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: FactoryProps,
+    codebuildRole: IRole
+  ) {
     super(scope, id);
 
-    const projectName = cdk.Stack.of(this).stackName
+    const projectName = cdk.Stack.of(this).stackName;
     // this is the source code to get github specs
     const gitHubSource = codebuild.Source.gitHub({
       owner: props.pipelineTemplateGithubOwner,
       repo: props.pipelineTemplateRepositoryName,
-      branchOrRef : props.pipelineTemplateBranchName,
+      branchOrRef: props.pipelineTemplateBranchName,
       webhook: false,
     });
 
-     // assumption about where the buildspec is located
+    // assumption about where the buildspec is located
     const buildSpecFile = "src/PipeLineTemplate/buildspec.json";
-    
-    
+
     const cdkCodeBuilder = new codebuild.Project(
       this,
       "CodeBuild_CreatePipeline",
@@ -28,11 +32,13 @@ export default class FactoryCodeBuildProject extends cdk.Construct {
         buildSpec: codebuild.BuildSpec.fromSourceFilename(buildSpecFile),
         source: gitHubSource,
         role: codebuildRole,
-        projectName : `${projectName}`
+        projectName: `${projectName}`,
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
+        },
       }
     );
-   
+
     this.BuildProject = cdkCodeBuilder;
   }
-
 }
